8f8872e9b219e3926eae06e6a33c110321f62a73
lukejans <91230392+lukejans@users.noreply.github.com>
2023-12-07 12:46:34 -0500

    Change game mode based on user selection

--- src/control.js
+++ src/control.js
@@ -5,20 +5,29 @@ const controller = (() => {
   /**
    * UI Dependency Injection
    *
-   * @param {gameBoard} array of each position on game board
-   * @param {components} object for toggling between ui screens
-   * @param {buttons} object for buttons in ui
+   * @param {gameBoard} - array of game board positions
+   * @param {components} - object of ui components to toggle
+   * @param {buttons} - object of buttons in ui
    */
   function init(gameBoard, components, buttons) {
+    // Game Board Move Handler
     gameBoard.forEach((box) => {
       box.addEventListener('click', function () {
         _handlePlayerClick(gameBoard, box);
       });
     });
-
+    // Start Button Handler
     buttons.start.addEventListener('click', function () {
       ui.toggleGameDisplay(components, buttons.start);
       console.log('game started');
+    });
+    // PvP Mode Selection
+    buttons.pvp.addEventListener('click', function () {
+      game.setMode('pvp');
+    });
+    // PvC Mode Selection
+    buttons.pvc.addEventListener('click', function () {
+      game.setMode('pvc');
     });
   }
 
--- src/game.js
+++ src/game.js
@@ -1,5 +1,28 @@
 const game = (() => {
-  return {};
+  /**
+   * Game Board
+   */
+  let _board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  /**
+   * Mode Selection
+   *
+   * get & set functions to change `_mode` based on
+   * buttons clicked from ui.
+   */
+  let _mode = '';
+
+  function getMode() {
+    return _mode;
+  }
+
+  function setMode(mode) {
+    _mode = mode;
+    console.log(mode);
+    return _mode;
+  }
+
+  return { getMode, setMode };
 })();
 
 export { game };
