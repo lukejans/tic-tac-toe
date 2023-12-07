b4304b8c538854111701871426f25e17462ef3d9
lukejans <91230392+lukejans@users.noreply.github.com>
2023-12-07 15:01:09 -0500

    Clean code

--- src/control.js
+++ src/control.js
@@ -10,13 +10,12 @@ const controller = (() => {
    * @param {buttons} - object of buttons in ui
    */
   function init(gameBoard, components, buttons) {
-    // Game Board Move Handler
     gameBoard.forEach((box) => {
       box.addEventListener('click', function () {
-        _handlePlayerClick(gameBoard, box);
+        _handlePlayerMoves(gameBoard, box);
       });
     });
-    // Start Button Handler
+
     buttons.start.addEventListener('click', function () {
       if (game.getMode() == '') {
         console.log('select a game mode to continue');
@@ -24,21 +23,17 @@ const controller = (() => {
       }
       game.buildPlayers();
       ui.toggleGameDisplay(components, buttons.start);
+
       console.log('game started');
       console.log({ player1: game.getPlayer(1), player2: game.getPlayer(2) });
     });
-    // PvP Mode Selection
+
     buttons.pvp.addEventListener('click', function () {
       game.setMode('pvp');
     });
-    // PvC Mode Selection
     buttons.pvc.addEventListener('click', function () {
       game.setMode('pvc');
     });
-  }
-
-  function _handlePlayerClick(gameBoard, box) {
-    // implement this function
   }
 
   return { init };
